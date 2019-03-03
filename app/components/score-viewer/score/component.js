df39d29e9eb8be1c04fcffdab24dec2006daece6
Brendan Fay <fay.brendan@gmail.com>
2019-03-03 18:43:41 -0500

    Fixes score cursor during play state.

--- app/components/score-viewer/score/component.js
+++ app/components/score-viewer/score/component.js
@@ -2,6 +2,9 @@ import Component from '@ember/component';
 
 export default Component.extend({
   classNames: ['score'],
+  classNameBindings: ['isPlaying:score--unclickable', 'isPreview:score--preview'],
+  isPlaying: false,
+  isPreview: false,
   showExtraBlankPage: false,
   sortedPages: null,
 });
